331a5cfab8b24dd98a581d1d75722e1e92e4e2c1
hfengGithub <hfeng@yahoo.com>
2016-05-23 23:05:14 -0500

    Delete updMPF5Price.sql

--- updMPF5Price.sql
+++
@@ -1,3 +0,0 @@
-UPDATE tblSecuritiesPalmsSource INNER JOIN tblFinalPriceSheet ON tblSecuritiesPalmsSource.CUSIP = tblFinalPriceSheet.Cusip 
-SET    tblSecuritiesPalmsSource.AggPrice = [New Price]
-WHERE (((tblSecuritiesPalmsSource.H1)="Seasoned"));
